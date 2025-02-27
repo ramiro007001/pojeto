2e5f3fb812824cda41276fe7397a6d8c1f141ece
ramiro007001 <ramirex9@gmail.com>
2025-02-27 03:25:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Olá, Render está rodando!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
+
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
